d1ea625bc1dcd6e1c1495c4e0bd7732d98213a75
stan <stanodin@gmail.com>
2016-01-16 04:33:43 +0200

    123

--- 2.rb
+++ 2.rb
@@ -3,42 +3,41 @@ puts text
 choise = ''
 while choise != 'x' do
   choise = gets.chomp
-
   choise_a = case choise
-  when 'w'
-    puts 'Enter your name'
-    name = gets.chomp
-    puts 'Enter your surname'
-    surname = gets.chomp
-    puts 'Enter your email'
-    emale = gets.chomp
-
-    names = [name, surname, emale]
-    puts 'Your data is:'
-    puts 'First Name, Last Name, Email'
-    puts names[0] + ', ' + names[1] + ', ' + names[2]
-
+    when 'w'
+    puts 'Enter your name; Last Name; Email'
+    data = gets.chomp
+    names = [data]
     filename = 'names.txt'
     File.open(filename, 'a') { |file| file.puts names }
+    puts 'Name | Last Name | Email'
+    File.open("names.txt") do |f|
+      while readline = f.gets
+        name, last_name, email = readline.chomp.split(";")
+        puts "#{name} #{last_name} #{email}"
+      end
+    end
     puts text
-  when 'r'
-    puts 'Your data is:'
-    filename = 'names.txt'
-    string = File.open(filename){|file| file.read}
-    puts string
+    when 'r'
+    puts 'Name | Last Name | Email'
+    File.open("names.txt") do |f|
+      while readline = f.gets
+        name, last_name, email = readline.chomp.split(";")
+        puts "#{name} #{last_name} #{email}"
+      end
+    end
     puts text
-  when 'd'
+    when 'd'
     puts 'Your data is deleted!'
     filename = 'names.txt'
     File.delete(filename)
     puts text
-  else
+    else
     puts 'Repeat'
     puts text
-  end
+    end
   puts choise_a
 end
-
 
 
 
